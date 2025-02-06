97d9a7407507970de8d917f54e9879be4d4a3d34
ARuslanDenisov <angess47@gmail.com>
2025-02-06 18:51:17 +0700

    121

---
+++ GetReadyForInterview/UserView.swift
@@ -0,0 +1,18 @@
+//
+//  USerView.swift
+//  GetReadyForInterview
+//
+//  Created by Руслан on 06.02.2025.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text("UserView")
+    }
+}
+
+#Preview {
+    UserView()
+}
