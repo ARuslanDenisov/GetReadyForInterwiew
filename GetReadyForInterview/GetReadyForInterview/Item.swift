798f49072887e0a8a31b011cd61a094704bb6bd3
ARuslanDenisov <angess47@gmail.com>
2025-02-06 15:35:32 +0700

    main

---
+++ GetReadyForInterview/GetReadyForInterview/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GetReadyForInterview
+//
+//  Created by Руслан on 06.02.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
