535c4ee97c1539ed97f6e334d78e3652d540759c
ARuslanDenisov <angess47@gmail.com>
2025-02-06 15:42:05 +0700

    1

---
+++ GetReadyForInterview/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GetReadyForInterview
+//
+//  Created by Руслан on 06.02.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MainView: View {
+   
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+#Preview {
+    MainView()
+}
