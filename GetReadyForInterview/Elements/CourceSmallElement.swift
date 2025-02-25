f5196c2e16593b87f1d50bd860560455dcabd611
ARuslanDenisov <angess47@gmail.com>
2025-02-25 14:55:49 +0700

    design change

---
+++ GetReadyForInterview/Elements/CourceSmallElement.swift
@@ -0,0 +1,18 @@
+//
+//  CourceSmallElement.swift
+//  GetReadyForInterview
+//
+//  Created by Руслан on 25.02.2025.
+//
+
+import SwiftUI
+
+struct CourceSmallElement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourceSmallElement()
+}
---
+++ GetReadyForInterview/Elements/GradientView.swift
@@ -0,0 +1,18 @@
+//
+//  GradientView.swift
+//  GetReadyForInterview
+//
+//  Created by Руслан on 25.02.2025.
+//
+
+import SwiftUI
+
+struct GradientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradientView()
+}
