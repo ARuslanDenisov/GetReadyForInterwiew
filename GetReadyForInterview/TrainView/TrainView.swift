75865da4d2e46d0e4a7a03114c14832b1fc064e3
ARuslanDenisov <angess47@gmail.com>
2025-02-26 00:11:27 +0700

    323

--- GetReadyForInterview/TrainView/TrainView.swift
+++ GetReadyForInterview/TrainView/TrainView.swift
@@ -9,71 +9,31 @@ import SwiftUI
 
 struct TrainView: View {
     @State private var scrollOffset: CGFloat = 0
+    @State var boolButton: Bool = true
     var body: some View {
         VStack {
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 380, height: 50)
-                    .foregroundStyle(.white)
-                    .shadow(color: .black.opacity(0.0 + Double(scrollOffset/150) ), radius: 4)
-                
-                HStack {
-                    Text("Тренировка")
-                        .font(.system(size: 32, weight: .bold, design: .rounded))
-                        .animation(.easeInOut)
+            Text("trainView")
+            Text("Выбор тем")
+            HStack {
+                Button {
+                    boolButton.toggle()
+                } label: {
                     Circle()
-                        .trim(from: 0, to: 0.7)
-                        .stroke(style: StrokeStyle(lineWidth: 3, lineCap: .round))
-                        .foregroundStyle(.black)
-                        .blur(radius: 1)
+                        .foregroundStyle(.redApp)
                     
-                    Spacer()
-                    HStack {
-                        // MARK: Search
-                        NavigationLink{
-                            
-                        }
-                        label:{
-                            SmallButonView("magnifyingglass", rad: 15)
-                                .frame(minWidth: 44, maxWidth: 44)
-                                .shadow(radius: 5)
-                        }
-                        // MARK: User
-                        NavigationLink{
-                            UserView()
-                        }
-                        label:{
-                            SmallButonView("person", rad: 15)
-                                .frame(minWidth: 44, maxWidth: 44)
-                                .shadow(radius: 5)
-                            
-                        }
-                    }
                 }
-                .frame(width: 350, height: 35)
-            }
-            .opacity(0.0 + Double(scrollOffset/100) )
-            ScrollView(.vertical, showsIndicators: false) {
-                GeometryReader { geometry in
-                    Text("")
-                        .preference(key: OffsetKey.self , value: geometry.frame(in: .global).minY)
-                }
-                .frame(height: 0)
-                .onPreferenceChange(OffsetKey.self) { value in
-                    // Обновляем смещение при изменении положения
-                    self.scrollOffset = value
-                }
-                VStack {
-                    ForEach(0..<7) { value in
-                        RoundedRectangle(cornerRadius: 20)
-                            .frame(width: 300, height: 200)
-                    }
-                }
+                .frame(width: 40, height: 40)
+                Text("1")
                 
-                Spacer()
             }
-            
-            
+            Text("выбор по кол-ву вопросов")
+            HStack{
+                Text("10")
+                Text("30")
+                Text("все вопросы")
+            }
+            Capsule()
+                .frame(width: 200, height: 20)
         }
     }
 }
---
+++ GetReadyForInterview/TrainView/TrainViewGame.swift
@@ -0,0 +1,18 @@
+//
+//  TrainViewGame.swift
+//  GetReadyForInterview
+//
+//  Created by Руслан on 25.02.2025.
+//
+
+import SwiftUI
+
+struct TrainViewGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrainViewGame()
+}
